f40d84022d78c17ffa66a24383413a83d53589ab
komawer02 <komawer02@naver.com>
2022-07-19 14:37:22 +0900

    secondcommit

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res)=>{
-    res.send('Hello world');
+    res.sendFile(__dirname + "/index.html")
 });
 
 app.listen(process.env.PORT || port, () => console.log("listening"));
